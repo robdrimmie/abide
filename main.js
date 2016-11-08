60cfdf8e6e3f3123e4da086fbf4f0a444197cb61
rmdrimmie <rob.drimmie@gmail.com>
2016-11-08 09:18:24 -0500

    Alright, all done.
    Getting and logging messages. Could in theory let this go for a while
    and it is fine though things like knowing who a user is and stuff would
    be pretty good too.

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 "use strict";
 
+const util = require('util');
 const nconf = require('nconf');
-nconf.file('./config.json');
-
 const slack = require('slack');
+
+nconf.file('./config.json');
 
 let bot = slack.rtm.client();
 let token = nconf.get("slack:token");
 
 bot.hello(message=> {
-  console.log(`Got a message: ${message}`)
-  bot.close()
+  console.log(util.inspect(message));
 })
 
-bot.listen({token})
+bot.message(message=> {
+  console.log(util.inspect(message));
+})
+
+bot.listen({token});
 
 console.log();
