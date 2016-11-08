4d23e2635a0d4c16eda7144d91d930072d84de06
rmdrimmie <rob.drimmie@gmail.com>
2016-11-08 07:33:21 -0500

    add slack. yay slack

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
+"use strict";
+
 const nconf = require('nconf');
 nconf.file('./config.json');
 
-console.log(nconf.get("slack:token"));
+const slack = require('slack');
+
+let bot = slack.rtm.client();
+let token = nconf.get("slack:token");
+
+bot.hello(message=> {
+  console.log(`Got a message: ${message}`)
+  bot.close()
+})
+
+bot.listen({token})
+
+console.log();
