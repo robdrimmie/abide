fef4729ee7d859d016bb4e92545f62fc43a3f57b
rmdrimmie <rob.drimmie@gmail.com>
2016-11-10 10:56:15 -0500

    output message and credit author

--- main.js
+++ main.js
@@ -9,12 +9,31 @@ nconf.file('./config.json');
 let bot = slack.rtm.client();
 let token = nconf.get("slack:token");
 
+let users = [];
+
+let output = (name, text)=> {
+    console.log( name + " said: " + text );
+}
+
 bot.hello(message=> {
   console.log(util.inspect(message));
 })
 
 bot.message(message=> {
-  console.log(util.inspect(message));
+  let name = "An unknown user";
+
+  if( void 0 === users[message.user]) {
+      slack.users.info({"token":token, "user":message.user},
+      (err,data) => {
+        //   console.log('looking up user');
+           console.log(err);
+          //console.log(data);
+          users[data.user.id] = data.user;
+          output(data.user.name, message.text);
+      });
+  } else {
+      output(users[message.user].name, message.text);
+  }
 })
 
 bot.listen({token});
