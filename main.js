b938042e6b0341fe3876f528d3c98321f34c2af7
rmdrimmie <rob.drimmie@gmail.com>
2016-11-11 16:16:33 -0500

    Resolve all properties of messages into human-readable things

--- main.js
+++ main.js
@@ -9,10 +9,126 @@ nconf.file('./config.json');
 let bot = slack.rtm.client();
 let token = nconf.get("slack:token");
 
+let teams = [];
+let channels = [];
 let users = [];
 
-let output = (name, text)=> {
-    console.log( name + " said: " + text );
+let resolveTeam = (promise) => {
+    let output = promise.output;
+
+    if (teams[promise.team]) {
+        output.team = teams[promise.team];
+        promise.output = output;
+
+        return promise;
+    }
+
+    return new Promise( function(resolve,reject) {
+      slack.team.info({"token":token, "team":promise.team}, (err,data) => {
+        if (err) {
+          throw err;
+        }
+
+        teams[data.team.id] = data.team;
+
+        output.team = teams[data.team.id];
+        promise.output = output;
+
+        resolve(promise);
+      });
+    });
+
+    throw new Error('resolve team did not return anything');
+}
+
+let resolveChannel = (promise) => {
+    let output = promise.output;
+
+    if (channels[promise.channel]) {
+        output.channel = channels[promise.channel];
+        promise.output = output;
+
+        return promise;
+    }
+
+    return new Promise( function(resolve,reject) {
+      slack.channels.info({"token":token, "channel":promise.channel}, (err,data) => {
+        let channelId;
+        if (err) {
+            if ('channel_not_found' === err) {
+              channelId = promise.channel.id;
+              channels[channelId] = {"name":"Unknown or DM"};
+            } else {
+              throw (err);
+            }
+        } else {
+          channelId = data.channel.id;
+          channels[channelId] = data.channel;
+        }
+
+        output.channel = channels[channelId];
+        promise.output = output;
+
+        resolve(promise);
+      });
+    });
+
+    throw new Error('resolve channel did not return anything');
+}
+
+let resolveUser = (promise) => {
+    let output = promise.output;
+
+    if (users[promise.user]) {
+        output.user = channels[promise.user];
+        promise.output = output;
+
+        return promise;
+    }
+
+    return new Promise( function(resolve,reject) {
+      slack.users.info({"token":token, "user":promise.user}, (err,data) => {
+        if (err) {
+          throw err;
+        }
+
+        users[data.user.id] = data.user;
+
+        output.user = users[data.user.id];
+        promise.output = output;
+
+        resolve(promise);
+      });
+    });
+
+    throw new Error('resolve user did not return anything');
+}
+
+let resolveTimestamp = (promise) => {
+    let output = promise.output;
+
+    output.timestamp = promise.ts;
+    promise.output = output;
+
+    return promise;
+}
+
+let resolveText = (promise) => {
+    let output = promise.output;
+
+    output.text = promise.text;
+    promise.output = output;
+
+    return promise;
+}
+
+let resolveOutput = (promise) => {
+    let output = "";
+    output += "[" + promise.output.team.name + "|" + promise.output.channel.name + "|" + promise.output.timestamp +"]\n";
+    output += promise.output.user.name + " said: " + promise.output.text;
+    console.log(output);
+
+    return promise;
 }
 
 bot.hello(message=> {
@@ -20,22 +136,18 @@ bot.hello(message=> {
 })
 
 bot.message(message=> {
-  let name = "An unknown user";
-
-  if( void 0 === users[message.user]) {
-      slack.users.info({"token":token, "user":message.user},
-      (err,data) => {
-        //   console.log('looking up user');
-           console.log(err);
-          //console.log(data);
-          users[data.user.id] = data.user;
-          output(data.user.name, message.text);
-      });
-  } else {
-      output(users[message.user].name, message.text);
-  }
+  message.output = {};
+  Promise.resolve(message)
+  .then(resolveTeam)
+  .then(resolveChannel)
+  .then(resolveUser)
+  .then(resolveTimestamp)
+  .then(resolveText)
+  .then(resolveOutput)
+  .catch((error) => {
+    console.log("chain broke:");
+    console.log(util.inspect(error));
+  });
 })
 
 bot.listen({token});
-
-console.log();
